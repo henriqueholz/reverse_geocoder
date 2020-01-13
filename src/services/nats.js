26284959822b1375e3d63e575207ba014a38f103
henriqueholz <henriqueholz@live.com>
2020-01-13 16:33:17 -0300

    commit

---
+++ src/services/nats.js
@@ -0,0 +1,13 @@
+const stan = require("node-nats-streaming").connect("test-cluster", "backend");
+
+let listenerEvents = io => {
+  stan.on("connect", function() {
+    let opts = stan.subscriptionOptions();
+    let subscription = stan.subscribe("buffer", opts);
+    subscription.on("message", function(message) {
+      io.emit("buffer", message.getData());
+    });
+  });
+};
+
+module.exports = listenerEvents;
